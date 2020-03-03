2832354282511e387b43fcdb2ae5aece0aa897cf
Amit Sagtani <asagtani06@gmail.com>
2020-03-03 14:01:43 +0530

    added lspci module

---
+++ transcribe/scribe_modules/lspci.py
@@ -0,0 +1,39 @@
+from . import ScribeModuleBaseClass
+
+import sys
+
+
+class Lspci(ScribeModuleBaseClass):
+
+    def __init__(self, input_dict=None, module_name=None, host_name=None,
+                 input_type=None, scribe_uuid=None):
+        ScribeModuleBaseClass.__init__(self, module_name=module_name,
+                                       input_dict=input_dict,
+                                       host_name=host_name,
+                                       input_type=input_type,
+                                       scribe_uuid=scribe_uuid)
+        if input_dict:
+            self.value = self._parse(input_dict)
+
+    def __iter__(self):
+        for attr, value in self.__dict__.items():
+            yield attr, value
+
+    def _parse(self, lspci_data):
+        output = []
+        lspci_lines = lspci_data.get("stockpile_lspci").split('\n\n')
+        if len(lspci_lines) <= 1:
+            print("Error occured in processing sysctl data")
+            sys.exit(1)
+
+        for i in range(len(lspci_lines)):
+            slot_data = lspci_lines[i].split('\n')
+            # sample - ['Slot:\t00:00.0', 'Class:\tHost bridge', 'Vendor:\tIntel Corporation' ..... ]
+            slot_dict = {}
+            for slot_info in slot_data:
+                key = slot_info.split(':\t')[0].strip()
+                value = slot_info.split(':\t')[1].strip()
+                slot_dict[key] = value
+            output.append(slot_dict)
+
+        return output
